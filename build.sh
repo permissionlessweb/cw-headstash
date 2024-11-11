e56e5a39b701b0b03cc8952bc4c7cf1705e13013
hard-nett <hardnettt@proton.me>
2024-11-11 16:11:38 -0500

    bump build docs

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ mv ./target/wasm32-unknown-unknown/release/cw_headstash.wasm ../public-crates/co
 mv ./target/wasm32-unknown-unknown/release/snip20_reference_impl.wasm ../public-crates/contracts/cw-glob/src/globs/snip20_reference_impl.wasm &&
 cat ../public-crates/contracts/cw-glob/src/globs/cw_headstash.wasm | gzip -9 > ../public-crates/contracts/cw-glob/src/globs/cw_headstash.wasm.gz &&
 cat ../public-crates/contracts/cw-glob/src/globs/snip20_reference_impl.wasm | gzip -9 > ../public-crates/contracts/cw-glob/src/globs/snip120u_impl.wasm.gz &&
+# sha256sum ../public-crates/contracts/cw-glob/src/globs/cw_headstash.wasm
+# sha256sum ../public-crates/contracts/cw-glob/src/globs/snip20_reference_impl.wasm
+# sha256sum ../public-crates/contracts/cw-glob/src/globs/cw_headstash.wasm.gz
+# sha256sum ../public-crates/contracts/cw-glob/src/globs/snip120u_impl.wasm.gz
 rm ../public-crates/contracts/cw-glob/src/globs/cw_headstash.wasm && rm ../public-crates/contracts/cw-glob/src/globs/snip20_reference_impl.wasm
 
 # 3. build public crates 
--- public-crates/contracts/cw-glob/README.md
+++ public-crates/contracts/cw-glob/README.md
@@ -2,3 +2,24 @@
 
 Stores compress wasm blobs for reference by an owner.  Only owner can call contract with string of wasm blob to reference, and contract response with blob in data of the response for uploading the desired blob (this example creates msgs for secret network wasm upload). 
 
+
+
+
+## Instantiate
+```sh
+'{"owner":"terp1..."}'
+```
+
+## Execute 
+
+### Hash Glob
+```sh
+'{"hash_glob":{"keys": ["cw-headstash", "snip120u"]}}'
+```
+
+### Take Glob
+```sh
+'{"take_glob":{"sender":"terp1...","key":"snip120u" }}'
+```
+
+## Query 
